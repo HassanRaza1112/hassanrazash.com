1f8d5afdf4031bd52f47cc7176c4eeac4d20448e
Hassan <aaron@yobo.dev>
2023-10-08 20:51:28 -0400

    Header Completed

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,8 @@
 
 import React from "react";
 import { motion } from "framer-motion";
+import { links } from "@/lib/data";
+import Link from "next/link";
 
 export default function Header() {
   return (
@@ -9,22 +11,33 @@ export default function Header() {
       <motion.div
         className="
         inset-x-0 max-w-max mx-auto
-        fixed top-3 rounded-xl 
+        fixed top-3 
+        rounded-xl 
         border border-white border-opacity-80 shadow-lg 
         shadow-black/[0.03] backdrop-blur-[0.5rem] 
-        px-5
-        py-2
+        bg-white bg-opacity-80
         "
         initial={{ y: -100, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
       >
-        <div className="flex justify-evenly">
-          <h3 className="p-2">Home</h3>
-          <h3 className="p-2">About</h3>
-          <h3 className="p-2">Projects</h3>
-          <h3 className="p-2">Skills</h3>
-          <h3 className="p-2">Contact</h3>
-        </div>
+        <ul className="flex justify-content items-center flex-wrap">
+          {links.map((link) => {
+            return (
+              <motion.li
+                className="h-3/4 flex justify-center items-center"
+                initial={{ y: -100, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+              >
+                <Link
+                  className="px-3 py-3 text-gray-500 hover:text-gray-950 transition"
+                  href={link.hash}
+                >
+                  {link.name}
+                </Link>
+              </motion.li>
+            );
+          })}
+        </ul>
       </motion.div>
     </header>
   );
---
+++ src/components/home.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import React, { Fragment } from "react";
+
+export const Home = () => {
+  return <section className="h-1/2">Home</section>;
+};
